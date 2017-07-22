5adca6f97615a3d2d4a347773091c3b89227b981
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2017-07-22 16:42:53 -0400

    Show the line items in the list

--- src/home/OrderList.jsx
+++ src/home/OrderList.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './OrderList.css';
 
+function printLineItems(lineItems) {
+  return lineItems.map(lineItem => `${lineItem.item.name} (${lineItem.quantity})`).join(',');
+}
+
 export default function OrderList({ orders, onOpenOrder }) {
   return (
     <table className="OrderList">
@@ -13,10 +17,10 @@ export default function OrderList({ orders, onOpenOrder }) {
             <td>#{order.id}</td>
             <td>{order.header.date}</td>
             <td>{order.header.name}</td>
-            <td>(amount due)</td>
+            <td>{order.isPaid ? 'paid' : '$xx.xx'}</td>
             <td>{order.header.patronCount || 0} people</td>
             <td>{order.header.deliveryMethod}</td>
-            <td>(items)</td>
+            <td>{printLineItems(order.lineItems)}</td>
           </tr>
           ),
         )}
